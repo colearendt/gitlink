5ff6d158c27088647c4f2799045d9deed7257add
Cole Arendt <cole.arendt@outlook.com>
2018-12-08 15:06:17 -0500

    add top right github ribbon

--- R/github.R
+++ R/github.R
@@ -1,11 +1,24 @@
 
 # https://blog.github.com/2008-12-19-github-ribbons/
+#' @export
 ribbon_github_top_left <- function(link) {
   htmltools::a(
     href = link,
     htmltools::img(
       style = "position: absolute; top: 0; left: 0; border: 0",
       src = "https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png",
+      alt = "Fork me on GitHub"
+    )
+  )
+}
+
+#' @export
+ribbon_github_top_right <- function(link, color = "white", hex = "ffffff") {
+  htmltools::a(
+    href = link,
+    htmltools::img(
+      style = "position: absolute; top: 0; right: 0; border: 0; z-index: 999",
+      src = paste0("https://s3.amazonaws.com/github/ribbons/forkme_right_", color, "_",hex,".png"),
       alt = "Fork me on GitHub"
     )
   )
