ce91f2cc31ad530b3f9a74ebc607cea2dc552b14
Cole Arendt <cole.arendt@outlook.com>
2019-01-03 11:52:51 -0500

    remove redundant functions

--- R/github.R
+++ R/github.R
@@ -126,18 +126,6 @@ ribbon_img <- function(link, position = "right", color = "white") {
   )
 }
 
-#' @export
-ribbon_github_top_left <- function(link) {
-  htmltools::a(
-    href = link,
-    htmltools::img(
-      style = "position: absolute; top: 0; left: 0; border: 0",
-      src = "https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png",
-      alt = "Fork me on GitHub"
-    )
-  )
-}
-
 color_lookup <- list(
   red = "aa0000",
   green = "007200",
@@ -146,17 +134,3 @@ color_lookup <- list(
   gray = "6d6d6d",
   white = "ffffff"
 )
-
-#' @export
-ribbon_github_top_right <- function(link, color = "white", hex = "ffffff") {
-  htmltools::a(
-    href = link,
-    htmltools::img(
-      style = "position: absolute; top: 0; right: 0; border: 0; z-index: 500",
-      src = paste0("https://s3.amazonaws.com/github/ribbons/forkme_right_", color, "_",hex,".png"),
-      alt = "Fork me on GitHub"
-    )
-  )
-}
-
-#<a href="https://github.com/you"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
