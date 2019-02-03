7138827cb840e2a326b4d465a8750a61be7b182a
Cole Arendt <cole.arendt@outlook.com>
2019-02-03 16:16:10 -0500

    add tests for css_ribbon

---
+++ tests/testthat/test-ribbon.R
@@ -0,0 +1,57 @@
+context("ribbon_css")
+
+test_that("link is passed correctly", {
+  res <- ribbon_css("https://thelink.example.com")
+  expect_match(as.character(res), 'href="https://thelink.example.com"')
+})
+
+test_that("style is included when needed", {
+  res <- ribbon_css("https://opacity.example.com", fade = TRUE)
+  expect_match(as.character(res), '<div class="ribbon-parent".*>.*</div>')
+  expect_match(as.character(res), '<style>.*</style>')
+
+  res_none <- ribbon_css("https://opacity.com", fade = FALSE)
+  not(expect_match)(as.character(res_none), '<style>.*</style>')
+})
+
+test_that("text is included", {
+  msg <- "Fork me Now... testing"
+  res <- ribbon_css("https://text.example.com", text = msg)
+  expect_match(as.character(res), paste0(">", msg, "</a>"))
+})
+
+test_that("position is configurable", {
+  res_left <- ribbon_css("https://left.example.com", position = "left")
+  expect_match(as.character(res_left), ";left:")
+  not(expect_match)(as.character(res_left), ";right:")
+
+  res_right <- ribbon_css("https://right.example.com", position = "right")
+  expect_match(as.character(res_right), ";right:")
+  not(expect_match)(as.character(res_right), ";left:")
+})
+
+test_that("rmarkdown render works", {
+  skip_if_not_installed("rmarkdown")
+  rmd <- rprojroot::find_testthat_root_file("rmd.Rmd")
+  output <- fs::file_temp("rmd", ext = ".html")
+  rmarkdown::render(rmd, output_file = output, quiet = TRUE)
+
+  res <- readLines(output)
+  expect_true(
+    any(
+      as.logical(
+        lapply(
+          res,
+          function(x){
+            grep("ribbon-parent", x)
+          }
+        )
+      )
+    )
+  )
+})
+
+test_that("app render works", {
+  skip_if_not_installed("shiny")
+  skip("test not yet implemented")
+})
