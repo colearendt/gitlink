ebb84f774ec707e75ce4750da5743eebebdee98f
Cole Arendt <cole.arendt@outlook.com>
2019-04-23 15:05:46 -0400

    add tests for ribbon_img

--- tests/testthat/test-ribbon.R
+++ tests/testthat/test-ribbon.R
@@ -63,3 +63,41 @@ test_that("app render works", {
   skip_if_not_installed("shiny")
   skip("test not yet implemented")
 })
+
+
+context("ribbon_img")
+
+test_that("works as expected", {
+  res <- ribbon_img("https://thelink.example.com")
+  expect_match(as.character(res), 'href="https://thelink.example.com"')
+  expect_match(as.character(res), "<img")
+})
+
+test_that("position is configurable", {
+  res_left <- ribbon_img("https://left.example.com", position = "left")
+  expect_match(as.character(res_left), "; left :")
+  expect_false(grepl("; right :", as.character(res_left)))
+
+  res_right <- ribbon_img("https://right.example.com", position = "right")
+  expect_match(as.character(res_right), "; right :")
+  expect_false(grepl("; left :", as.character(res_right)))
+})
+
+test_that("fails on bad position", {
+  skip("not implemented")
+  expect_error(
+    ribbon_img("https://invalid.example.com", position = "invalid"),
+    "invalid"
+  )
+})
+
+
+test_that("color_lookup succeeds", {
+
+  for (color in get_color_lookup()) {
+    color_val <- get_color_lookup()[[color]]
+    res_color <- ribbon_img("https://color.example.com", color = color)
+    expect_match(as.character(res_color), paste(color, color_val, sep = "_"))
+
+  }
+})
