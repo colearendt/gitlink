7975ad83c775516592eea66d52eb77de5f17661f
Cole Arendt <cole.arendt@outlook.com>
2019-04-23 08:30:02 -0400

    add img import

--- R/gitlink-package.R
+++ R/gitlink-package.R
@@ -1,6 +1,7 @@
 #' @importFrom htmltools a
 #' @importFrom htmltools css
 #' @importFrom htmltools div
+#' @importFrom htmltools img
 #' @importFrom htmltools tags
 #' @importFrom htmltools tagList
 #' @importFrom rlang list2
