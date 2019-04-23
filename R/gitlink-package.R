ac580aedee41d8b54f1037852ef141d3e6855686
Cole Arendt <cole.arendt@outlook.com>
2019-04-23 08:29:26 -0400

    add imports

--- R/gitlink-package.R
+++ R/gitlink-package.R
@@ -1,2 +1,8 @@
+#' @importFrom htmltools a
+#' @importFrom htmltools css
+#' @importFrom htmltools div
+#' @importFrom htmltools tags
+#' @importFrom htmltools tagList
+#' @importFrom rlang list2
 #' @keywords internal
 "_PACKAGE"
